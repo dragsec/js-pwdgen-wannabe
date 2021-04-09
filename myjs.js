39698facd02e3d45d93d36ccd3e575a4f8dc9200
dragsec <draganyes@gmail.com>
2021-04-09 06:59:13 +0200

    finale1.0

---
+++ myjs.js
@@ -0,0 +1,5 @@
+var nome = prompt('Quale è il tuo nome?');
+var cognome = prompt('Quale è il tuo cognome?');
+var colorep = prompt('Quale è il tuo colore preferito?');
+var num = 21;
+document.getElementById("stampa").innerHTML =nome + cognome + colorep + num;
